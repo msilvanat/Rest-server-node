6327fb46ec03fa259a524c248ba70aaed0b22292
Silvana <msilvanat@gmail.com>
2021-02-26 21:27:52 -0300

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,30 +3,31 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
 
     const { correo, password } = req.body;
 
-    try { 
+    try {
         //Verificar si el email existe
         const usuario = await Usuario.findOne({ correo });
-        if( !usuario ) {
+        if (!usuario) {
             return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - correo'
             });
         }
 
         //Si el usuario está activo en la DB
-        if( !usuario.estado ) { 
+        if (!usuario.estado) {
             return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - estado: false'
             });
         }
 
         //Verificar la contraseña
-        const validPassword = bcryptjs.compareSync( password, usuario.password );
-        if( !validPassword ) {
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
             return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - password'
             });
@@ -35,8 +36,8 @@ const login = async (req, res = response) => {
 
 
         //Generar el JWT
-        const token = await generarJWT( usuario.id );
-        
+        const token = await generarJWT(usuario.id);
+
         res.json({
             usuario,
             token
@@ -53,6 +54,55 @@ const login = async (req, res = response) => {
     }
 }
 
+
+const googleSignin = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en BD
+        if( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+    }
+
+}
+
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
